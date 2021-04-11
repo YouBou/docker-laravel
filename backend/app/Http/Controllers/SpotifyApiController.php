5c33f075791f8245a961530ac37ef4f266636680
YouBou <ikas2lite@gmail.com>
2021-04-11 14:59:27 +0900

    [feat] auth Spotify API

--- backend/app/Http/Controllers/SpotifyApiController.php
+++ backend/app/Http/Controllers/SpotifyApiController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
+use SpotifyWebAPI;
 
 class SpotifyApiController extends BaseController
 {
     public function index()
     {
-        require '/backend/app/vendor/autoload.php';
+        require __DIR__.'/../../../vendor/autoload.php';
 
         $session = new SpotifyWebAPI\Session(
-            
+            env('SPOTIFY_API_CLIENT_ID'),
+            env('SPOTIFY_API_CLIENT_SECRET'),
+            'http://0.0.0.0:80/spotify'
         );
 
         $api = new SpotifyWebAPI\SpotifyWebAPI();
@@ -19,19 +22,19 @@ class SpotifyApiController extends BaseController
         if (isset($_GET['code'])) {
             $session->requestAccessToken($_GET['code']);
             $api->setAccessToken($session->getAccessToken());
-        
+
         } else {
             header('Location: ' . $session->getAuthorizeUrl(array(
-                'scope' => array( 
-                  'playlist-read-private', 
-                  'playlist-modify-private', 
+                'scope' => array(
+                  'playlist-read-private',
+                  'playlist-modify-private',
                   'user-read-private',
                   'playlist-modify'
                 )
             )));
             die();
         }
-        
+
         echo '<pre>';
             print_r($api->me()); //認証を受けたアカウントのプロフィールが表示される
         echo '</pre>';
